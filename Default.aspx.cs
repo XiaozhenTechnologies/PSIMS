800af4b34131242d78fdb202d8b946f5493240f7
Shengbo Kong <kongshengbo@gmail.com>
2016-03-06 10:05:16 +0800

    test
    test

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -4,11 +4,30 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
 
 public partial class _Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
         Console.WriteLine("asdfasdfasdfsdaf");
+        Response.Write("test");
+
+        String sql = "Select * from Users";
+
+        MySqlConnection conn = SqlHelper.Connect();
+        MySqlCommand command = new MySqlCommand(sql, conn);
+        MySqlDataReader reader = command.ExecuteReader();
+
+        while(reader.Read())
+        {
+            if (reader.HasRows)
+            {
+                Response.Write("/" + reader.GetString(0) + "/" + reader.GetString(1));
+            }
+        }
+
+        reader.Close();
+        conn.Close();
     }
 }
